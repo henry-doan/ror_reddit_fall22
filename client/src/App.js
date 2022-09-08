ec87da22d52eb23e5c6008e33362ecf3cefa60ba
Henry Doan <henrydoan96@gmail.com>
2022-09-07 20:33:33 -0600

    all sub CRUD done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import Subs from './components/subs/Subs';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
 
 const App = () => (
-  <>
+  <>  
+    <Home />
+    <About />
     <Subs />
   </>
 )
